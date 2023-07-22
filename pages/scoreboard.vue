a9a33424b60b835e56195c1f6b904a93dba661c6
Isis Logullo <isis.logullo@gmail.com>
2023-07-21 22:52:56 -0300

    insercao paginas

---
+++ pages/scoreboard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="canvas">
+		<div id="deep-space">
+			<h1
+				class="custom-title"
+			>
+				Placar
+			</h1>
+
+			<div class="button-stack">
+				<NuxtLink
+					class = "custom-nuxtlink"
+				>
+					Voltar
+				</NuxtLink>
+			</div>
+		</div>
+	</div>
+</template>
